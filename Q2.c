4f248695ebfe43e4bad8c4118f6ccbc88355e5ff
DaviMaiaF <121465789+DaviMaiaF@users.noreply.github.com>
2023-11-09 14:02:28 -0300

    Envio da questão 2

---
+++ Q2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+// Função para calcular o valor do montante ao fim de cada mês
+double calcularMontanteMensal(double aporte, double taxaJuros, int tempo) {
+    double montante;
+    montante = aporte * (1 + taxaJuros) * ((pow((1 + taxaJuros), tempo) - 1) / taxaJuros);
+    return montante;
+}
+
+int main() {
+    double aporte, taxaJuros;
+    int tempo;
+
+    // Entrada de dados
+    printf("Digite o tempo em meses: ");
+    scanf("%d", &tempo);
+    
+    printf("Digite o valor do aporte mensal: ");
+    scanf("%lf", &aporte);
+
+    printf("Digite a taxa de juros mensal (em decimal): ");
+    scanf("%lf", &taxaJuros);
+
+
+    // Cálculo e exibição do montante ao fim de cada mês
+    for (int mes = 1; mes <= tempo; mes++) {
+        double montante = calcularMontanteMensal(aporte, taxaJuros, mes);
+        printf("Montante ao fim do mes%3d: R$ %.2f\n", mes, montante);
+    }
+
+    return 0;
+}
+
